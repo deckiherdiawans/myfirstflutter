939c352d6d5e871604213037422df6ea2a174a83
Decki Herdiawan Soepandi <d.herdiawan.s@gmail.com>
2022-07-22 17:03:25 +0700

    First basic tutorial finished.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Welcome to Flutter!',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter!'),
-        ),
-        body: const Center(
-          child: Text('What\'s up motherfuckers!!!'),
-        ),
+      home: RandomWords(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final suggestions = <WordPair>[];
+  final biggerFont = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+      ),
+      body: buildSuggestions(),
+    );
+  }
+
+  Widget buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (BuildContext context, int i) {
+        if (i.isOdd) {
+          return const Divider();
+        }
+
+        final int index = i ~/ 2;
+
+        if (index >= suggestions.length) {
+          suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: biggerFont,
       ),
     );
   }
